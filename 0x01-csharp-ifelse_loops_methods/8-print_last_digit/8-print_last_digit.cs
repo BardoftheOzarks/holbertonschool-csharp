e98988ae0334a6e1f7de224ebb0cde024d6804e9
BardoftheOzarks <1886@holbertonschool.com>
2021-07-13 18:42:13 -0500

    Added tasks 7 thru 9

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0)
+            number = number * -1;
+        number = number % 10;
+        Console.Write("{0}", number);
+        return number;
+    }
+}
