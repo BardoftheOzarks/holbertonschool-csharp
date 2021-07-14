c7d4fd344e92b4b139627c557248d71cf42f065a
BardoftheOzarks <1886@holbertonschool.com>
2021-07-14 17:10:52 -0500

    Added tasks 5-9

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count > 0)
+        {
+            int biggest = myList[0];
+            foreach (int nummer in myList)
+                if (nummer > biggest)
+                    biggest = nummer;
+            return biggest;
+        }
+        Console.WriteLine("List is empty");
+        return -1;
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (myList.Count > index)
+            myList.Remove(myList[index]);
+        else
+            Console.WriteLine("Index is out of range");
+        return myList;
+    }
+}
