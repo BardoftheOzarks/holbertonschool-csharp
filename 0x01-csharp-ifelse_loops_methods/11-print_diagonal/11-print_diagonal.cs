a02f68185d64d37cf26a8babf2ab073f7267e26b
BardoftheOzarks <1886@holbertonschool.com>
2021-07-13 19:25:09 -0500

    Added tasks 10 thru 12

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = i; j > 0; --j)
+                Console.Write(' ');
+            Console.WriteLine('\\');
+        }
+        Console.Write('\n');
+    }
+}
