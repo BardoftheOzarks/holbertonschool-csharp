8d7b78cc07dabf02a9b1b5e375580b55d52c6488
BardoftheOzarks <1886@holbertonschool.com>
2021-07-14 18:14:37 -0500

    Added tasks 10-13

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        SortedList<string, string> sortedDict = new SortedList<string, string>();
+        foreach (KeyValuePair<string, string> pair in myDict)
+            sortedDict[pair.Key] = pair.Value;
+        foreach (KeyValuePair<string, string> pair in sortedDict)
+            Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int best = 0;
+        string name = "";
+        foreach(KeyValuePair<string, int> pair in myList)
+        {
+            if (best < pair.Value)
+            {
+                best = pair.Value;
+                name = pair.Key;
+            }
+        }
+        return name;
+    }
+}
