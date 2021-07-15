8aeb98621b2e1d9b62b4f8aa88a9f27fca910cfb
BardoftheOzarks <1886@holbertonschool.com>
2021-07-15 15:23:17 -0500

    Added tasks 0-4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniqueList = new HashSet<int>();
+        foreach (int num in myList)
+            uniqueList.Add(num);
+        int sum = 0;
+        foreach (int num in uniqueList)
+            sum = sum + num;
+        return sum;
+    }
+}
