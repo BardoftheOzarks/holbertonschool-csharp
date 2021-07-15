eb006da2a88ecbb1bb622c3f5484a663a7e7a9ec
BardoftheOzarks <1886@holbertonschool.com>
2021-07-15 16:14:20 -0500

    Added tasks 5-9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myList)
+    {
+        int sum = 0;
+        foreach (var element in myList)
+            sum++;
+        return sum;
+    }
+}
