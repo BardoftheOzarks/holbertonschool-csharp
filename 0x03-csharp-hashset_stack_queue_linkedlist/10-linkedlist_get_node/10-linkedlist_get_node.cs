f101527282685596f51a2eda2c3739703e19df70
BardoftheOzarks <1886@holbertonschool.com>
2021-07-16 07:31:12 -0500

    dimished for loop

--- 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -8,7 +8,7 @@ class LList
         if (n >= 0 && n < myLList.Count)
         {
             LinkedListNode<int> scanner = myLList.First;
-            for (int i = 0; i <= n; i++)
+            for (int i = 0; i < n; i++)
                 scanner = scanner.Next;
             return scanner.Value;
         }
