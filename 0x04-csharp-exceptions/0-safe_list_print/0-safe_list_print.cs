5d8f098839731dd14544c3760ed10a6f13443ae3
BardoftheOzarks <1886@holbertonschool.com>
2021-07-26 18:35:53 -0500

    Initial push + task 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            try
+            {
+                Console.WriteLine(myList[i]);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return i;
+            }
+        }
+        return n;
+    }
+}
---
+++ 0x04-csharp-exceptions/README.md
@@ -0,0 +1,1 @@
+Directory for Exceptions project tasks
