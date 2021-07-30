74a606d7052422a8fa01e18c97e73cc3cda0235c
BardoftheOzarks <1886@holbertonschool.com>
2021-07-30 18:30:06 -0500

    Added task 3

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -1,18 +1,19 @@
 using NUnit.Framework;
 
-namespace Tests
+namespace Text.Tests
 {
+    [TestFixture]
+    /// <summary>Test cases for 3-palindrome</summary>
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Assert.IsTrue(Str.IsPalindrome("level"));
+            Assert.IsTrue(Str.IsPalindrome("Racecar"));
+            Assert.IsTrue(Str.IsPalindrome("A man, a plan, a canal: Panama."));
+            Assert.IsTrue(Str.IsPalindrome(""));
+            Assert.IsFalse(Str.IsPalindrome("Hello"));
         }
     }
 }
--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -2,7 +2,23 @@
 
 namespace Text
 {
-    public class Class1
+    public class Str
     {
+        public static bool IsPalindrome(string s)
+        {
+            if (s.Length > 0)
+            {
+                for (int l = 0, r = s.Length - 1; l < r; l++, r--)
+                {
+                    while (!Char.IsLetter(s[l]))
+                        l++;
+                    while (!Char.IsLetter(s[r]))
+                        r--;
+                    if (Char.ToLower(s[l]) != Char.ToLower(s[r]))
+                        return false;
+                }
+            }
+            return true;
+        }
     }
 }
