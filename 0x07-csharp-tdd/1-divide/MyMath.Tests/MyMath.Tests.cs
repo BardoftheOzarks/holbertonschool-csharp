1776bd770192941a10cec1658946260f5e27ac37
BardoftheOzarks <1886@holbertonschool.com>
2021-07-30 00:01:59 -0500

    missing a ;

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -13,7 +13,7 @@ namespace MyMath.Tests
             int[,] testMatrix = new int[,]{{0, 2, 4}, {6, 8, 10}, {12, 14, 16}};
             int[,] halvedMatrix = new int[,]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
             Assert.AreEqual(Matrix.Divide(testMatrix, 0), null);
-            Assert.AreEqual(Matrix.Divide(testMatrix, 2), halvedMatrix)
+            Assert.AreEqual(Matrix.Divide(testMatrix, 2), halvedMatrix);
         }
     }
 }
