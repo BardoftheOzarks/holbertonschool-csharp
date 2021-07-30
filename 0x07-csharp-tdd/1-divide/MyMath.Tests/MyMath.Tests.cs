c4f47f0289c2b56a8a39f7eb60e14cb2c73261e5
BardoftheOzarks <1886@holbertonschool.com>
2021-07-30 00:04:26 -0500

    added null matrix test

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -14,6 +14,7 @@ namespace MyMath.Tests
             int[,] halvedMatrix = new int[,]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
             Assert.AreEqual(Matrix.Divide(testMatrix, 0), null);
             Assert.AreEqual(Matrix.Divide(testMatrix, 2), halvedMatrix);
+            Assert.AreEqual(Matrix.Divide(null, 0), null);
         }
     }
 }
