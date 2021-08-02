437e0d1df1482429c30744413c79af68fb377de4
BardoftheOzarks <1886@holbertonschool.com>
2021-08-02 18:20:08 -0500

    Added tasks 2 + 3

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Host class to test an object</summary>
+class Obj
+{
+    /// <summary>Determines if an derivedType inherits but is not an instance of baseType</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary>Host class to test objects</summary>
+class Obj
+{
+    /// <summary>Method to print names of properties and methods of an object</summary>
+    public static void Print(object myObj)
+    {
+        var type = myObj.GetType();
+        Console.WriteLine($"{type.GetTypeInfo().Name} Properties:");
+        foreach (var obj in type.GetProperties())
+        {
+            Console.WriteLine(obj.Name);
+        }
+        Console.WriteLine($"{type.GetTypeInfo().Name} Methods:");
+        foreach (var obj in type.GetMethods())
+        {
+            Console.WriteLine(obj.Name);
+        }
+    }
+}
