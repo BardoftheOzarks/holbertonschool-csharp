cc0a513e21e65173be91763e8dc7ec61ae866b0a
BardoftheOzarks <1886@holbertonschool.com>
2021-08-02 12:21:21 -0500

    Initial push with tasks 0 & 1

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Host class to test an object</summary>
+class Obj
+{
+    /// <summary>Tests if an object is an int</summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int)
+            return true;
+        return false;
+    }
+}
---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Host class to test an object</summary>
+class Obj
+{
+    /// <summary>Tests if an object is or inherits from Array</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array)
+            return true;
+        return false;
+    }
+}
