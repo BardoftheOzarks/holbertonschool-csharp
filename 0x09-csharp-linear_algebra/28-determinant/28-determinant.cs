22fbf54edf65874d7959b009670515ae142ac0a6
BardoftheOzarks <1886@holbertonschool.com>
2021-08-22 09:59:50 -0500

    created determinant algorithm

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != matrix.GetLength(1))
+            return -1;
+        
+        if (matrix.GetLength(0) > 2)
+        {
+            double sum = 0;
+            for (int a = 0; a < matrix.GetLength(0); a++)
+            {
+                double[,] smaller = new double[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+                double factor = matrix[0, a];
+                if (a % 2 == 1)
+                    factor *= -1;
+                for (int b = 0; b < matrix.GetLength(0) - 1; b++)
+                    for (int c = 0; c < matrix.GetLength(1); c++)
+                    {
+                        if (c == a)
+                            continue;
+                        else if (c > a)
+                            smaller[b, c - 1] = matrix[b + 1, c];
+                        else
+                            smaller[b, c] = matrix[b + 1, c];
+                    }
+                sum += factor * Determinant(smaller);
+            }
+            return sum;
+        }
+        double ans = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        return ans;
+    }
+}
---
+++ 0x09-csharp-linear_algebra/28-determinant/28-main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] M = new double[3, 2] {{1, 2}, {3, 4}, {5, 6}};
+        double[,] N = new double[2, 3] {{1, 2, 3}, {4, 5, 6}};
+        double[,] O = new double[2, 2] {{2, 2}, {-9, 4}};
+        double[,] P = new double[3, 3] {{-4, 9, 0}, {1, -2, 1}, {3, -4, 2}};
+        Console.WriteLine(MatrixMath.Determinant(M) + "\n");
+        Console.WriteLine(MatrixMath.Determinant(N) + "\n");
+        Console.WriteLine(MatrixMath.Determinant(O) + "\n");
+        Console.WriteLine(MatrixMath.Determinant(P) + "\n");
+        Console.WriteLine(MatrixMath.Determinant(new double[,] {{}}) + "\n");
+    }
+}
