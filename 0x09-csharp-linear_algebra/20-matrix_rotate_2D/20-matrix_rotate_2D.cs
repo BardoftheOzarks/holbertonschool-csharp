c446912d5b3c4df81dbb66e1f4b8624a899eff98
BardoftheOzarks <1886@holbertonschool.com>
2021-08-22 02:02:05 -0500

    made null factor matrix negative

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -7,13 +7,13 @@ class MatrixMath
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
         if (matrix.GetLength(1) != 2)
-            return new double[1, 1] {{1}};
+            return new double[1, 1] {{-1}};
         
         double[,] ans = new double[matrix.GetLength(0), matrix.GetLength(1)];
         for (int row = 0; row < matrix.GetLength(0); row++)
         {
-            ans[row, 0] = Math.Round((matrix[row, 0] * Math.Cos(angle)) - (matrix[row, 1] * Math.Sin(angle)), 3);
-            ans[row, 1] = Math.Round((matrix[row, 0] * Math.Sin(angle)) + (matrix[row, 1] * Math.Cos(angle)), 3);
+            ans[row, 0] = Math.Round((matrix[row, 0] * Math.Cos(angle)) - (matrix[row, 1] * Math.Sin(angle)), 2);
+            ans[row, 1] = Math.Round((matrix[row, 0] * Math.Sin(angle)) + (matrix[row, 1] * Math.Cos(angle)), 2);
         }
         return ans;
     }
