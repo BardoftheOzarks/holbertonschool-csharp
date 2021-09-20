5f6a503accac8e20b41ea239d7d75b8ba73fb6eb
BardoftheOzarks <1886@holbertonschool.com>
2021-09-19 20:35:28 -0500

    All methods converted to BitLock (see readme)

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -11,17 +11,9 @@ public class ImageProcessor
     ///<summary>inverts the colorscale of images</summary>
     public static void Inverse(string[] filenames)
     {
-        //foreach (string file in filenames)
-        //    CreateInverse(file);
-
-        //string[] current = Directory.GetFiles("./", "*.jpg");
-        //string[] images = Directory.GetFiles("images/", "*.jpg");
-        //int total = current.Length + images.Length;
         Parallel.ForEach (filenames, file => {
             CreateInverse(file);
         });
-        //while (current.Length != total)
-        //    current = Directory.GetFiles("./", "*.jpg");
     }
 
     private static void CreateInverse(string file)
@@ -68,49 +60,45 @@ public class ImageProcessor
     ///<summary>Recreates an image in grayscale</summary>
     public static void Grayscale(string[] filenames)
     {
-        //foreach (string file in filenames)
-        //{
-        //    CreateGrayscale(file);
-        //}
-        string[] current = Directory.GetFiles("./", "*.jpg");
-        string[] images = Directory.GetFiles("images/", "*.jpg");
-        int total = current.Length + images.Length;
         Parallel.ForEach (filenames, file => {
             CreateGrayscale(file);
         });
-        //while (current.Length != total)
-        //    current = Directory.GetFiles("./", "*.jpg");
     }
 
     private static void CreateGrayscale(string file)
     {
-        //read image
         Bitmap bmp = new Bitmap(file);
+        BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
 
-        //get image dimensions
-        int width = bmp.Width;
-        int height = bmp.Height;
+        int bytesPerPixel = Bitmap.GetPixelFormatSize(bmp.PixelFormat) / 8;
+        int byteCount = bitmapData.Stride * bmp.Height;
+        byte[] pixels = new byte[byteCount];
+        IntPtr ptrFirstPixel = bitmapData.Scan0;
+        Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
+        int heightInPixels = bitmapData.Height;
+        int widthInBytes = bitmapData.Width * bytesPerPixel;
 
-        //change each pixel to inverse
-        for (int y = 0; y < height; y++)
+        for (int y = 0; y < heightInPixels; y++)
         {
-            for (int x = 0; x < width; x++)
+            int currentLine = y * bitmapData.Stride;
+            for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
             {
-                //get pixel values
-                Color pixel = bmp.GetPixel(x, y);
+                int oldBlue = pixels[currentLine + x];
+                int oldGreen = pixels[currentLine + x + 1];
+                int oldRed = pixels[currentLine + x + 2];
 
-                //extract specific values
-                int red = pixel.R;
-                int green = pixel.G;
-                int blue = pixel.B;
-
-                //get average
-                int avg = (red + green + blue) / 3;
-
-                //set new values to pixel
-                bmp.SetPixel(x, y, Color.FromArgb(avg, avg, avg));
+                // calculate new pixel value
+                int avg = (oldBlue + oldGreen + oldRed) / 3;
+                pixels[currentLine + x] = (byte)avg;
+                pixels[currentLine + x + 1] = (byte)avg;
+                pixels[currentLine + x + 2] = (byte)avg;
             }
         }
+ 
+        // copy modified bytes back
+        Marshal.Copy(pixels, 0, ptrFirstPixel, pixels.Length);
+        bmp.UnlockBits(bitmapData);
+
         //create new file name
         string name = string.Format("{0}_grayscale{1}",
                                     Path.GetFileNameWithoutExtension(file),
@@ -122,59 +110,49 @@ public class ImageProcessor
     ///<summary>Reproduces image with only black and white pixels based on set threshold</summary>
     public static void BlackWhite(string[] filenames, double threshold)
     {
-        //foreach (string file in filenames)
-        //{
-        //    CreateBlackWhite(file, threshold);
-        //}
-        string[] current = Directory.GetFiles("./", "*.jpg");
-        string[] images = Directory.GetFiles("images/", "*.jpg");
-        int total = current.Length + images.Length;
         Parallel.ForEach (filenames, file => {
             CreateBlackWhite(file, threshold);
         });
-        while (current.Length != total)
-            current = Directory.GetFiles("./", "*.jpg");
     }
     
     private static void CreateBlackWhite(string file, double threshold)
     {
-        //read image
         Bitmap bmp = new Bitmap(file);
+        BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
 
-        //get image dimensions
-        int width = bmp.Width;
-        int height = bmp.Height;
+        int bytesPerPixel = Bitmap.GetPixelFormatSize(bmp.PixelFormat) / 8;
+        int byteCount = bitmapData.Stride * bmp.Height;
+        byte[] pixels = new byte[byteCount];
+        IntPtr ptrFirstPixel = bitmapData.Scan0;
+        Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
+        int heightInPixels = bitmapData.Height;
+        int widthInBytes = bitmapData.Width * bytesPerPixel;
 
-        //change each pixel to inverse
-        for (int y = 0; y < height; y++)
+        for (int y = 0; y < heightInPixels; y++)
         {
-            for (int x = 0; x < width; x++)
+            int currentLine = y * bitmapData.Stride;
+            for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
             {
-                //get pixel values
-                Color pixel = bmp.GetPixel(x, y);
+                int oldBlue = pixels[currentLine + x];
+                int oldGreen = pixels[currentLine + x + 1];
+                int oldRed = pixels[currentLine + x + 2];
 
-                //extract specific value
-                int red = pixel.R;
-                int green = pixel.G;
-                int blue = pixel.B;
-
-                //obtain average
-                int avg = (red + green + blue) / 3;
-
-                //divide black and white pixels
-                if (avg >= threshold)
-                {
+                // calculate new pixel value
+                int avg = (oldBlue + oldGreen + oldRed) / 3;
+                if ((double)avg > threshold)
                     avg = 255;
-                }
                 else
-                {
                     avg = 0;
-                }
-
-                //set new values to pixel
-                bmp.SetPixel(x, y, Color.FromArgb(avg, avg, avg));
+                pixels[currentLine + x] = (byte)avg;
+                pixels[currentLine + x + 1] = (byte)avg;
+                pixels[currentLine + x + 2] = (byte)avg;
             }
         }
+ 
+        // copy modified bytes back
+        Marshal.Copy(pixels, 0, ptrFirstPixel, pixels.Length);
+        bmp.UnlockBits(bitmapData);
+
         //create new file name
         string name = string.Format("{0}_bw{1}",
                                     Path.GetFileNameWithoutExtension(file),
@@ -183,21 +161,13 @@ public class ImageProcessor
         bmp.Save(name);
     }
 
+
     ///<summary>Creates a thumbnail of an image at the specified height</summary>
     public static void Thumbnail(string[] filenames, int height)
     {
-        //foreach (string file in filenames)
-        //{
-        //    CreateThumbnail(file, height);
-        //}
-        string[] current = Directory.GetFiles("./", "*.jpg");
-        string[] images = Directory.GetFiles("images/", "*.jpg");
-        int total = current.Length + images.Length;
         Parallel.ForEach (filenames, file => {
             CreateThumbnail(file, height);
         });
-        while (current.Length != total)
-            current = Directory.GetFiles("./", "*.jpg");
     }
 
     private static void CreateThumbnail(string file, int newHeight)
--- image_processor/README.md
+++ image_processor/README.md
@@ -4,3 +4,5 @@ Inverse inverts an image's color.
 Grayscale removes all color from an image.
 BlackWhite renders an image using only black and white pixels (no grey);
 Thumbnail scales the image to the input height in pixels.
+
+Resource: http://csharpexamples.com/fast-image-processing-c/
