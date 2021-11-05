3891fac90e6a3abe818f3b0c99ad57406f5238ef
BardoftheOzarks <1886@holbertonschool.com>
2021-11-04 23:38:25 -0500

    Added all console functions

--- 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
+++ 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
@@ -8,7 +8,6 @@ class InventoryManager
         {"baseclass", typeof(BaseClass)},
         {"user", typeof(User)},
         {"item", typeof(Item)},
-        {"inventory", typeof(Inventory)}
     };
     string prompt = "Inventory Manager\n-------------------------\n<ClassNames> show all ClassNames of objects\n<All> show all objects\n<All [ClassName]> show all objects of a ClassName\n<Create [ClassName]> a new object\n<Show [ClassName object_id]> an object\n<Update [ClassName object_id]> an object\n<Delete [ClassName object_id]> an object\n<Exit>\n";
 
@@ -58,13 +57,19 @@ class InventoryManager
                         Console.WriteLine("Usage: Create <ClassName> <id>");
                     return;
                 case "show":
-                    Show(args, storage);
+                    if (args.Length == 3)
+                        Show(args[1], args[2]);
+                    else
+                        Console.WriteLine("Usage: Show <ClassName> <id>");
                     return;
                 case "update":
-                    Update(args, storage);
+                    if (args.Length == 4)
+                        Update(args[1], args[2], args[3]);
+                    else
+                        Console.WriteLine("Usage: Update <ClassName> <id> <update>");
                     return;
                 case "delete":
-                    Delete(args, storage);
+                    Delete(args[1], args[2]);
                     return;
                 case "exit":
                     Exit();
@@ -90,17 +95,72 @@ class InventoryManager
         }
     }
 
-    public void Create(string name)
+    public void Create(string ClassName, string name)
     {
-        if (classes.ContainsKey(name))
+        if (classes.ContainsKey(ClassName))
         {
-            switch (name)
+            switch (ClassName)
             {
                 case "item":
-
+                    storage.New(new Item(name));
+                case "user":
+                    storage.New(new User(name));
+                case "baseclass":
+                    storage.New(new BaseClass(name));
             }
         } else {
-            Console.WriteLine($"{name} is not a valid object");
+            Console.WriteLine($"{ClassName} is not a valid object");
         }
+    }
+
+    public void Show(string ClassName, string id)
+    {
+        if (classes.ContainsKey(ClassName))
+        {
+            foreach (string key in objects.Keys)
+            {
+                if (key.Split('.')[1].ToLower() == ClassName && key.Split('.')[2] == id)
+                    Console.WriteLine(objects[key].ToString());
+            }
+        } else {
+            Console.WriteLine($"{ClassName} is not a valid object class");
+        }
+    }
+
+    public void Update(string ClassName, string id, string update)
+    {
+        if (classes.ContainsKey(ClassName))
+        {
+            foreach (string key in objects.Keys)
+            {
+                if (key.Split('.')[1].ToLower() == ClassName && key.Split('.')[2] == id)
+                {
+                    objects[key].name = update;
+                }
+            }
+        } else {
+            Console.WriteLine($"{ClassName} is not a valid object class");
+        }
+    }
+    public Delete(string ClassName, string id)
+    {
+        if (classes.ContainsKey(ClassName))
+        {
+            foreach (string key in objects.Keys)
+            {
+                if (key.Split('.')[1].ToLower() == ClassName && key.Split('.')[2] == id)
+                {
+                    objects.Remove(key);
+                    objects.Save();
+                }
+            }
+        } else {
+            Console.WriteLine($"{ClassName} is not a valid object class");
+        }
+    }
+    
+    public void Exit()
+    {
+        System.Environment.Exit(1);
     }
 }
