ea3c4f5b1c5ce3441f1d9e648b1dd9372f01901f
BardoftheOzarks <62080409+BardoftheOzarks@users.noreply.github.com>
2022-03-21 19:53:53 -0500

    Delete 0x0D-csharp-text_based_interface directory

--- 0x0D-csharp-text_based_interface/InventoryLibrary/Inventory.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-///<summary>Inventory Class</summary>
-class Inventory : BaseClass
-{
-    public string user_id;
-    public string item_id;
-    public int quantity;
-}
--- 0x0D-csharp-text_based_interface/README.md
+++
@@ -1,1 +0,0 @@
-Directory for Inventory Management project tasks
