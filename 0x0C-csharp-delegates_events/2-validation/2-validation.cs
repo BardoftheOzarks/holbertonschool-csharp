bb442603781951322e37c6e3561e16cb2c55d9f9
BardoftheOzarks <1886@holbertonschool.com>
2022-03-21 20:14:42 -0500

    Revert "Delete 0x0C-csharp-delegates_events directory"
    This reverts commit 8fa2b14cb52ae2736363ca394c0498bfb5c37958.

---
+++ 0x0C-csharp-delegates_events/2-validation/2-validation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    public float hp;
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        } else {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+    public delegate void CalculateHealth(float amount);
+
+    public void TakeDamage(float damage)
+    {
+        if (damage <= 0)
+            Console.WriteLine($"{this.name} takes 0 damage!");
+        else
+        {
+            Console.WriteLine($"{this.name} takes {damage} damage!");
+            this.hp -= damage;
+        }
+        ValidateHP(this.hp);
+    }
+    public void HealDamage(float heal)
+    {
+        if (heal <= 0)
+            Console.WriteLine($"{this.name} heals 0 HP!");
+        else
+        {
+            Console.WriteLine($"{this.name} heals {heal} HP!");
+            this.hp += heal;
+        }
+        ValidateHP(this.hp);
+    }
+    
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+            this.hp = 0;
+        else if (newHp > this.maxHp)
+            this.hp = maxHp;
+        else
+            this.hp = newHp;
+    }
+}
